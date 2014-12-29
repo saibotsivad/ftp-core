1f8120eeb1c7e0ea838a87524bff22da8d45309e
Tobias Davis <accounts@davistobias.com>
2014-12-29 00:11:53 -0600

    Reorganizing and implementing some basic features.

--- README.md
+++ README.md
@@ -4,6 +4,62 @@ A minimal, hopefully unopinionated implementation of FTP in JavaScript.
 
 This is a work-in-progress.
 
+Ideally, this will provide *only* the minimum framework to have a
+functional FTP server, and if you wanted to implement another common
+FTP extension it'd be really easy.
+
+Not sure what sort of hooks are required, at the moment.
+
+The goal is to not use a FS for the backend, but to wire it up
+so you pass in a levelup object.
+
+Then, there'd be an FS-based levelup db, so you can use that if
+you want a normal FTP server off the filesystem.
+
+If you check out the server-behaviour, you'll see it's a thin wrapper
+on an emitter to take a "commands" object.
+
+Currently that "commands" object is like this:
+
+	{
+		COMMAND: function(args, callback) {}
+	}
+
+Now the `args` is what you'd get from the client, so for example
+if the FTP server got this command
+
+	USER anonymous
+
+This would call the "commands" object `USER` function, and `args`
+would simply be `anonymous`.
+
+It's up to each command function to parse and interpret the argument
+string.
+
+At the end of doing anything, the callback is given a number of arguments
+like this:
+
+	callback(230)
+
+or
+
+	callback(230 'welcome to my server')
+
+or even
+
+	callback(230, 'first line of message', 'second line of message')
+
+And this is interpreted into the appropriate string to be given
+back to the client, formatted according to the FTP specs.
+
+## get involved?
+
+you could make issues, if you think of something
+
+or you could try contributing code, if you've got some free time
+
+I'm pretty easy to work with
+
 ## License
 
 Released under the [VOL](http://veryopenlicense.com).
--- test/test-core.js
+++ test/test-core.js
@@ -1,12 +1,13 @@
 var test = require('tape')
+  , returnCodes = require('ftp-return-codes')
+  , EventEmitter = require('events').EventEmitter
 var serverBehaviour = require('../server-behaviour')
-var serverBehaviorObject = require('../commands')
-var generateLines = require('../generate-lines')
-var EventEmitter = require('events').EventEmitter
-var returnCodes = require('ftp-return-codes')
+  , generateLines = require('../generate-lines')
+  , ServerCommands = require('../server-commands')
 
-test('whatever basic first', function(t) {
+test('that the USER command emits correctly', function(t) {
 	var clientEmitter = new EventEmitter()
+	var serverBehaviorObject = ServerCommands({})
 	var server = serverBehaviour(serverBehaviorObject, clientEmitter, returnCodes)
 
 	t.plan(2)
@@ -18,5 +19,74 @@ test('whatever basic first', function(t) {
 		t.end()
 	})
 
-	clientEmitter.emit('USER', 'JohnDoe')
+	clientEmitter.emit('USER', 'anonymous')
 })
+
+test('that the FEAT command emits correctly', function(t) {
+	var clientEmitter = new EventEmitter()
+	var serverBehaviorObject = ServerCommands({})
+	var server = serverBehaviour(serverBehaviorObject, clientEmitter, returnCodes)
+
+	t.plan(2)
+
+	server.on('response', function(responseNumber, message) {
+		t.equals(responseNumber, 211)
+		t.equals(message, generateLines(211, [ 'Features:', 'USER', 'FEAT', 'PWD', 'CWD', 'End' ]))
+		server.stop()
+		t.end()
+	})
+
+	clientEmitter.emit('FEAT')
+})
+
+test('that the PWD command emits correctly', function(t) {
+	var clientEmitter = new EventEmitter()
+	var serverBehaviorObject = ServerCommands({})
+	var server = serverBehaviour(serverBehaviorObject, clientEmitter, returnCodes)
+
+	t.plan(2)
+
+	server.on('response', function(responseNumber, message) {
+		t.equals(responseNumber, 257)
+		t.equals(message, generateLines(257, '"/" is current directory.'))
+		server.stop()
+		t.end()
+	})
+
+	clientEmitter.emit('PWD')
+})
+
+test('that the CWD command emits correctly', function(t) {
+	var clientEmitter = new EventEmitter()
+	var serverBehaviorObject = ServerCommands({})
+	var server = serverBehaviour(serverBehaviorObject, clientEmitter, returnCodes)
+
+	t.plan(2)
+
+	server.on('response', function(responseNumber, message) {
+		t.equals(responseNumber, 257)
+		t.equals(message, generateLines(257, '"/test" is current directory.'))
+		server.stop()
+		t.end()
+	})
+
+	clientEmitter.emit('CWD', '/test')
+})
+
+test('that the CWD command emits correctly for missing folder paths', function(t) {
+	var clientEmitter = new EventEmitter()
+	var serverBehaviorObject = ServerCommands({})
+	var server = serverBehaviour(serverBehaviorObject, clientEmitter, returnCodes)
+
+	t.plan(2)
+
+	server.on('response', function(responseNumber, message) {
+		t.equals(responseNumber, 250)
+		t.equals(message, '250 Broken client detected, missing argument to CWD. "/" is current directory.\r\n')
+		server.stop()
+		t.end()
+	})
+
+	clientEmitter.emit('CWD')
+})
+

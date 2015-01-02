d5b9e402b21c3c57fb2f81649a69e33ef93d5452
Josh Duff <me@JoshDuff.com>
2015-01-02 17:00:41 -0600

    Deleting commented-out code

--- ftp.js
+++ ftp.js
@@ -17,59 +17,14 @@ module.exports = function FtpCore(controlEmitter, dataSocketEmitterCreator) {
 
 	ftp.extend = extend.bind(null, ftp)
 
-
-
-
-	// 	createDataConnection: function createDataConnection(ipAddress, port, callback) {
-	// 		dataSocketEmitterCreator(ipAddress, port, function(emitter) {
-	// 			ftp.dataConnectionEmitter = emitter
-	// 			callback()
-	// 		})
-	// 	}
-	// }
-
-	// ftp.commands.USER = function(userName, callback) {
-	// 	if (userName === 'anonymous') {
-	// 		ftp.userName = 'anonymous'
-	// 		ftp.userIsAuthenticated = true
-	// 		callback(230, 'Logged in as anonymous user.')
-	// 	} else {
-	// 		callback(530, 'User not logged in.', 'This server only accepts anonymous users.', 'Please use "USER anonymous".')
-	// 	}
-	// }
-
-	// ftp.commands.PWD = function(ignore, callback) {
-	// 	if (ftp.userIsAuthenticated) {
-	// 		callback(257, '"' + ftp.currentDirectory + '" is current directory.')
-	// 	} else {
-	// 		callback(530, 'User not logged in.')
-	// 	}
-	// }
-
-	// ftp.commands.CWD = function(directory, callback) {
-	// 	if (ftp.userIsAuthenticated) {
-	// 		ftp.directoryExists(directory, function(exists) {
-	// 			if (exists) {
-	// 				ftp.currentDirectory = directory
-	// 				ftp.commands.PWD(null, callback)
-	// 			} else {
-	// 				callback(250, 'Broken client detected, missing argument to CWD. "' + ftp.currentDirectory + '" is current directory.')
-	// 			}
-	// 		})
-	// 	} else {
-	// 		callback(530, 'User not logged in.')
-	// 	}
-	// }
-
 	return ftp
 }
-
 
 function extend(ftp, extension) {
 	if (!extension) {
 		throw new Error('extension must be defined')
 	}
-	
+
 	console.log(ftp, extension)
 	var newFtp = {
 		commands: extendObject(Object.create(ftp.commands), extension.commands || {}),
